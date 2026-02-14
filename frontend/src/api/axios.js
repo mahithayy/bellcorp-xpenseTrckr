a49ab0a24febaba59a43be3323bdaa8d1f6b6541
mahithayy <hollamahitha@gmail.com>
2026-02-14 11:52:30 +0530

    Fix API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mahi-bellcorp-xpensetrckr.onrender.com/",
+  baseURL: "https://mahi-bellcorp-xpensetrckr.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
